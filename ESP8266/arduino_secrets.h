30419ca9240890d3f810cfc213b41bb212f7981b
Andrés David Gómez Bautista <36377557+gomezan@users.noreply.github.com>
2022-04-17 18:09:50 -0500

    codigo clase

--- ESP8266/arduino_secrets.h
+++ ESP8266/arduino_secrets.h
@@ -1,6 +1,6 @@
-#define USERNAME "gomezan"
-#define DEVICE_ID "DHT"
-#define DEVICE_CREDENTIAL "wJPgc+OdYpK-?NjZ"
+#define USERNAME ""
+#define DEVICE_ID ""
+#define DEVICE_CREDENTIAL ""
 
 #define SSID "Usaca"
 #define SSID_PASSWORD "Z1p4@v13j0"
